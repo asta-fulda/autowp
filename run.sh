2ab13a9f15ba87b50dc4965e757ecfb49d03c500
Alexander Wellbrock <alexander.wellbrock@asta.hs-fulda.org>
2017-08-05 11:28:34 +0200

    Changing small typo

--- run.sh
+++ run.sh
@@ -8,6 +8,7 @@ docker network create runtime
 printf "Network created!\nCreating MYSQL-Container...\n"
 docker run --rm --name=wpdb --net runtime -e MYSQL_ROOT_PASSWORD=password -e MYSQL_DATABASE=wordpress -e MYSQL_USER=wordpress -e MYSQL_PASSWORD=password -d mysql:5.6
 printf "Waiting for the DB-Container to be ready...\n"
+printf "Waiting for DB-Container to become ready...\n"
 sleep 60
 printf "DB ready!\nStarting Wordpress Instance...\n"
 docker run --rm --network runtime -p 80:80 --name wp -d astafulda/wp-base:1.0-apache
