e5f210a2c7566d011c5ccc0fb807d0470da00819
Alexander Wellbrock <alexander.wellbrock@asta.hs-fulda.org>
2017-08-14 22:49:56 +0200

    New image structure

--- build-apache.sh
+++ build-apache.sh
@@ -10,13 +10,16 @@ printf "Done.\nCreating database container...\n"
 docker rm -f wpdb
 docker run --rm --name=wpdb --net build_context -e MYSQL_ROOT_PASSWORD=password -e MYSQL_DATABASE=wordpress -e MYSQL_USER=wordpress -e MYSQL_PASSWORD=password -d mysql:5.6
 printf "Done.\nBuilding modified apache base image...\n"
-cd wp-cli-php7.1-apache/
-docker build -t astafulda/wp-cli-php7.1-apache .
-printf "Done.\nWaiting for the database container to become ready...\n"
-sleep 30
+cd wp-cli/
+docker build -t astafulda/wp-cli .
+#printf "Done.\nWaiting for the database container to become ready...\n"
+#sleep 30
+cd ../wp
+printf "Done.\nBuilding wordpress apache image...\n"
+docker build --net build_context -t astafulda/wp .
 cd ../wp-base
 printf "Done.\nBuilding wordpress base image...\n"
-docker build --no-cache --network build_context -t astafulda/wp-base:1.0-apache .
+docker build --net build_context -t astafulda/wp-base .
 printf "Done.\nStopping database container...\n"
 docker rm -f wpdb
 printf "Done.\nRemoving build_context network...\n"
